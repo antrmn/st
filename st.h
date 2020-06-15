e4b86054ffaca3617e58e253f3d2adf8e9e0b47f
Antonio Romano <antonio@x240>
2020-06-15 21:07:18 +0200

    add osc patch

--- st.h
+++ st.h
@@ -134,5 +134,6 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
+extern unsigned int defaultcs;
 extern const int boxdraw, boxdraw_bold, boxdraw_braille;
 extern float alpha;
