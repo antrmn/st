1ef140d4d077fb3d1f3866082529cef49fe84745
Antonio Romano <antonio@x240>
2020-06-16 12:12:37 +0200

    add copyurl patch

--- config.def.h
+++ config.def.h
@@ -263,6 +263,7 @@ static Shortcut shortcuts[] = {
     { TERMMOD,              XK_Return,      newterm,        {.i =  0} },
     { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
     { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+    { MODKEY,               XK_l,           copyurl,        {.i =  0} },
 };
 
 /*
