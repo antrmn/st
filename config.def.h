6b1a2a358581bafdd3d18188daefc6d28640ca07
Antonio Romano <antonio@x240>
2020-06-16 12:37:01 +0200

    Revert "add opencopied patch"
    This reverts commit 5a71f3febfabd858384e8f9cf0a33c38ab28e1ea.

--- config.def.h
+++ config.def.h
@@ -264,7 +264,6 @@ static Shortcut shortcuts[] = {
     { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
     { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
     { MODKEY,               XK_l,           copyurl,        {.i =  0} },
-    { MODKEY,               XK_o,           opencopied,     {.v =  "xdg-open"} },
 };
 
 /*
